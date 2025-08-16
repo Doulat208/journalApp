7053eb902eccacb8a7fe80794c3c256f06b20a83
Doulat Biswal <doulatbiswal209@gmail.com>
2025-08-16 23:15:50 +0530

    Our Journal App Project Setup

---
+++ src/main/java/com/myspringboot/journalApp/JournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+}
